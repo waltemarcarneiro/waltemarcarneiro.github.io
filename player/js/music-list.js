ef9430c270d17f1d2918503876385b24c0f7c920
Waltemar Carneiro <78711339+waltemarcarneiro@users.noreply.github.com>
2023-09-08 04:13:13 -0400

    Add files via upload

--- player/js/music-list.js
+++ player/js/music-list.js
@@ -3,43 +3,43 @@ let allMusic = [
   {
     name: "The Lady In Red",
     artist: "Chris De Burgh",
-    img: "images/music-1.jpg",
+    img: "./covers/cover-1.jpg",
     src: "https://archive.org/download/chris-de-burgh-lady-in-red/Chris%20DeBurgh%20%20Lady%20In%20Red.mp3"
   },
   {
     name: "Hazard",
     artist: "Richard Marx",
-    img: "images/music-2.jpg",
+    img: "./covers/cover-2.jpg",
     src: "https://archive.org/download/richard-marx-hazard/Richard-Marx-Hazard.mp3"
   },
   {
     name: "Hero",
     artist: "Mariah carey",
-    img: "images/music-3.jpg",
+    img: "./covers/cover-3.jpg",
     src: "https://archive.org/download/mariah-carey-hero_202308/Mariah-Carey-Hero.mp3"
   },
   {
     name: "There’s Never a Forever Thing",
     artist: "A-ha",
-    img: "images/music-4.jpg",
+    img: "./covers/cover-4.jpg",
     src: "http://archive.org/download/a-ha-theres-never-a-forever-thing/A-HA-There's-Never-A-Forever-Thing.mp3"
   },
   {
     name: "Amazing",
     artist: "Aerosmith",
-    img: "images/music-5.jpg",
+    img: "./covers/cover-5.jpg",
     src: "http://archive.org/download/a-ha-theres-never-a-forever-thing/AEROSMITH-Amazing.mp3"
   },
   {
     name: "Ordinary World",
     artist: "Duran Duran",
-    img: "images/music-6.jpg",
+    img: "./covers/cover-6.jpg",
     src: "https://archive.org/download/duran-duran-ordinary-world_202309/Duran-Duran_Ordinary_World.mp3"
   },
   {
     name: "Breathe",
     artist: "Faith Hill",
-    img: "images/music-7.jpg",
+    img: "./covers/cover-7.jpg",
     src: "https://archive.org/download/duran-duran-ordinary-world_202309/Faith-Hill_Breathe.mp3"
   },
 
