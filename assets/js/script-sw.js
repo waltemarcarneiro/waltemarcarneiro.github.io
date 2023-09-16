121f8d72f105f1b476147fa2d2d19dfb78b0cb99
Waltemar Carneiro <78711339+waltemarcarneiro@users.noreply.github.com>
2023-09-16 02:02:03 -0400

    Update script-sw.js

--- assets/js/script-sw.js
+++ assets/js/script-sw.js
@@ -1,10 +1,14 @@
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker
-    .register('/assets/js/service-worker.js')
-    .then(serviceWorker => {
-      console.log('Service Worker registered: ' + serviceWorker);
-    })
-    .catch(error => {
-      console.log('Error registering the Service Worker: ' + error);
-    });
-}
+if ("serviceWorker" in navigator) {
+      if (navigator.serviceWorker.controller) {
+        console.log("active service worker found, no need to register");
+      } else {
+        // Register the service worker
+        navigator.serviceWorker
+          .register("service-worker.js", {
+            scope: "/"
+          })
+          .then(function (reg) {
+            console.log("Service worker has been registered for scope: " + reg.scope);
+          });
+      }
+    }
