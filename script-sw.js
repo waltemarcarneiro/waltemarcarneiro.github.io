7234952530579085ca47d8d78d30ca3c16ef4213
Waltemar Carneiro <78711339+waltemarcarneiro@users.noreply.github.com>
2023-09-20 01:26:01 -0400

    Add files via upload

--- script-sw.js
+++ script-sw.js
@@ -1,14 +1,20 @@
 if ("serviceWorker" in navigator) {
-      if (navigator.serviceWorker.controller) {
-        console.log("active service worker found, no need to register");
-      } else {
-        // Register the service worker
-        navigator.serviceWorker
-          .register("service-worker.js", {
-            scope: "/"
-          })
-          .then(function (reg) {
-            console.log("Service worker has been registered for scope: " + reg.scope);
-          });
-      }
-    }
+  if (navigator.serviceWorker.controller) {
+    console.log("Active service worker found, no need to register");
+  } else {
+    // Register the service worker
+    navigator.serviceWorker
+      .register("service-worker.js", {
+        scope: "/"
+      })
+      .then(function (reg) {
+        console.log("Service worker has been registered for scope: " + reg.scope);
+      });
+  }
+
+  // Check for updates to the Service Worker
+  navigator.serviceWorker.addEventListener("controllerchange", function () {
+    // Reload the page to activate the new Service Worker
+    window.location.reload();
+  });
+}
