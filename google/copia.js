3ebf3269745bfb9c88e2ed13754bb26706883c4c
Waltemar Carneiro <78711339+waltemarcarneiro@users.noreply.github.com>
2023-09-30 01:41:28 -0400

    Add files via upload

---
+++ google/copia.js
@@ -0,0 +1,28 @@
+//copia texto
+        document.addEventListener('DOMContentLoaded', function () {
+            var botaoCopiar = document.getElementById('botaoCopiar');
+            var textoParaCopiar = document.getElementById('textoParaCopiar');
+
+            botaoCopiar.addEventListener('click', function () {
+                // Seleciona o texto dentro da strong
+                var texto = textoParaCopiar.textContent;
+
+                // Cria um elemento de input temporário para copiar o texto
+                var inputTemporario = document.createElement('input');
+                inputTemporario.setAttribute('type', 'text');
+                inputTemporario.setAttribute('value', texto);
+                document.body.appendChild(inputTemporario);
+
+                // Seleciona o texto no elemento de input temporário
+                inputTemporario.select();
+
+                // Executa o comando de cópia
+                document.execCommand('copy');
+
+                // Remove o elemento de input temporário
+                document.body.removeChild(inputTemporario);
+
+                // Atualiza o texto do botão após a cópia
+                botaoCopiar.textContent = 'Senha Copiada! 🥰 Aguarde...';
+            });
+        });
