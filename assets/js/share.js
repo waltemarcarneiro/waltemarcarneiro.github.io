8945c47fdad9be32e9c65e1ca3533e2eb25b86e9
Waltemar Carneiro <78711339+waltemarcarneiro@users.noreply.github.com>
2023-12-07 01:57:54 -0400

    Add files via upload

---
+++ assets/js/share.js
@@ -0,0 +1,23 @@
+
+   // Adicione o evento de clique ao ícone de seta à esquerda
+        document.getElementById('backButton').addEventListener('click', () => {
+            // Volta para a página anterior
+            history.back();
+        });
+   // Adicione o evento de clique ao ícone de compartilhamento
+        document.getElementById('shareButton').addEventListener('click', () => {
+            // Compartilha a página atual
+            if (navigator.share) {
+                navigator.share({
+                    title: document.title,
+                    url: window.location.href
+                }).then(() => {
+                    console.log('Página compartilhada com sucesso!');
+                }).catch((error) => {
+                    console.error('Erro ao compartilhar a página:', error);
+                });
+            } else {
+                console.log('Compartilhamento não suportado pelo navegador.');
+                // Aqui você pode adicionar um fallback ou mensagem para navegadores que não suportam o compartilhamento nativo.
+            }
+        });        
