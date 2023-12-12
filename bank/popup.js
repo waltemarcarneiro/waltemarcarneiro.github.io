97fcbb7347d08f0ffc96aebae949004827bada77
Waltemar Carneiro <78711339+waltemarcarneiro@users.noreply.github.com>
2023-12-12 00:36:55 -0400

    Add files via upload

--- bank/popup.js
+++ bank/popup.js
@@ -1,20 +1,27 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const openButton = document.getElementById('open-button');
-    const popupOverlay = document.getElementById('popup-overlay');
-    const popup = document.getElementById('popup');
-    const closeButton = document.getElementById('close-button');
+        document.addEventListener('DOMContentLoaded', () => {
+            const openButton = document.getElementById('open-button');
+            const popupOverlay = document.getElementById('popup-overlay');
+            const popup = document.getElementById('popup');
+            const closeButton = document.getElementById('close-button');
 
-    
-    closeButton.addEventListener('click', () => {
-        popupOverlay.style.display = 'none'; // Esconde o popup
-    });
+            closeButton.addEventListener('click', () => {
+                popupOverlay.style.display = 'none'; // Esconde o popup
+            });
 
-    popupOverlay.addEventListener('click', (event) => {
-        if (event.target === popupOverlay) {
-            popupOverlay.style.display = 'none';
-        }
-    });
+            popupOverlay.addEventListener('click', (event) => {
+                if (event.target === popupOverlay) {
+                    popupOverlay.style.display = 'none';
+                }
+            });
 
-    // Mostra automaticamente o popup ao carregar a página
-    popup.style.display = 'block';
-});
+            // Verifica se o popup já foi exibido
+            const popupShown = localStorage.getItem('popupShown');
+
+            if (!popupShown) {
+                // Mostra automaticamente o popup ao carregar a página
+                popupOverlay.style.display = 'flex';
+                // Marca que o popup foi exibido
+                localStorage.setItem('popupShown', 'true');
+            }
+        });
+   
