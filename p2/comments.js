dd005e65da654912c12fb643ea974aac13f3c0d6
Waltemar Carneiro <78711339+waltemarcarneiro@users.noreply.github.com>
2024-07-05 12:12:10 -0400

    Delete p2 directory

--- p2/comments.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('post-comment').addEventListener('click', function() {
-    let comment = document.getElementById('comment').value;
-    if (comment) {
-        let commentElement = document.createElement('div');
-        commentElement.textContent = comment;
-        document.getElementById('comment-list').appendChild(commentElement);
-        document.getElementById('comment').value = '';
-    }
-});
