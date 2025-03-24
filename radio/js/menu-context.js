4189fbfd0d204ea84a98a3df2e4f450e7a91d4b0
Waltemar Carneiro <78711339+waltemarcarneiro@users.noreply.github.com>
2025-03-24 11:39:45 -0400

    Add files via upload

---
+++ radio/js/menu-context.js
@@ -0,0 +1,5 @@
+
+        // Prevenir o menu de contexto ao segurar o dedo na tela
+        document.addEventListener('contextmenu', function(event) {
+            event.preventDefault(); // Impede o menu de contexto
+        });
