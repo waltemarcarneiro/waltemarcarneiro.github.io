1d4ff18dfbd1f105b69e5471ee7cdeeb9953ea91
waltemarcarneiro <waltemarcarneiro@users.noreply.github.com>
2025-05-07 16:53:02 -0400

    Update no home.html

--- push.sh
+++ push.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-# Carrega variáveis do .env
+# Carrega variáveis do .env (opcional se quiser manter para outras configs)
 source .env
 
 # CONFIGURAÇÕES
 GITHUB_USER="waltemarcarneiro"
 REPO_NAME="waltemarcarneiro.github.io"
 BRANCH="main"
-TOKEN="$GITHUB_TOKEN"
 
 # Mensagem de commit
 COMMIT_MSG=${1:-"Atualização automática"}
@@ -19,7 +18,7 @@ git config user.email "$GITHUB_USER@users.noreply.github.com"
 # Inicializa o repositório se ainda não tiver .git
 if [ ! -d .git ]; then
     git init
-    git remote add origin https://$GITHUB_USER:$TOKEN@github.com/$GITHUB_USER/$REPO_NAME.git
+    git remote add origin git@github.com:$GITHUB_USER/$REPO_NAME.git
 fi
 
 # Add, commit e push
@@ -29,5 +28,5 @@ if git diff --cached --quiet; then
     echo "Nenhuma mudança para commitar."
 else
     git commit -m "$COMMIT_MSG"
-    git push https://$GITHUB_USER:$TOKEN@github.com/$GITHUB_USER/$REPO_NAME.git $BRANCH
+    git push origin $BRANCH
 fi
