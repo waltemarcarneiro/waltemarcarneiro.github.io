1ef3f90e39c288461ea43bd8aaff4cda621f7774
waltemarcarneiro <waltemarcarneiro@users.noreply.github.com>
2025-05-15 19:19:56 -0400

    update files

--- components/modals/modal.js
+++ components/modals/modal.js
@@ -1,13 +1,7 @@
-function showModalLogin() {
-    fetch('/components/modals/modalLogin.html')
-        .then(response => response.text())
-        .then(html => {
-            document.body.insertAdjacentHTML('beforeend', html);
-        });
-}
+// ...existing code...
 
-function showModalSantander() {
-    fetch('/components/modals/modalSantander.html')
+function openSantander() {
+    fetch('./components/modals/modalSantander.html')
         .then(response => response.text())
         .then(html => {
             document.body.insertAdjacentHTML('beforeend', html);
@@ -15,5 +9,31 @@ function showModalSantander() {
 }
 
 // Tornando as funções disponíveis globalmente
-window.showModalLogin = showModalLogin;
-window.showModalSantander = showModalSantander;
+window.openSantander = openSantander;
+
+// Funções do modalSantander
+window.copiarCodigo = function(codigo) {
+    const elemento = event.currentTarget;
+    const mensagem = elemento.dataset.message;
+    navigator.clipboard.writeText(codigo).then(() => {
+        mostrarMensagem(mensagem);
+    });
+}
+
+window.copiarChave = function(chave) {
+    const elemento = event.currentTarget;
+    const mensagem = elemento.dataset.message;
+    navigator.clipboard.writeText(chave).then(() => {
+        mostrarMensagem(mensagem);
+    });
+}
+
+window.mostrarMensagem = function(texto) {
+    const mensagem = document.getElementById('mensagem');
+    mensagem.innerHTML = texto;
+    mensagem.style.display = 'block';
+    
+    document.addEventListener('click', function() {
+        mensagem.style.display = 'none';
+    }, { once: true });
+}
