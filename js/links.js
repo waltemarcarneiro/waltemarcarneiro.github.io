34bd3c5f4661f5ddbb986edb7a1520af87f2bc10
Waltemar Carneiro <waltemarcarneiro@gmail.com>
2025-05-31 21:22:24 -0400

    update filesgp

--- js/links.js
+++ js/links.js
@@ -4,13 +4,15 @@ function abrirPix() {
     window.location.href = "bank/santander.html";
 }
 
-
 function abrirWhatsApp() {
     window.location.href = "https://api.whatsapp.com/send?phone=5581971027858&text=%F0%9F%98%80%20Ol%C3%A1%2C%20estou%20com%20d%C3%BAvidas!";
 }
 
 function abrirProfile() {
     window.location.href = "./pages/profile.html";
+}
+function vivoRecarga() {
+    window.location.href = "https://recarga.vivo.com.br/mobile/home";
 }
 
 function abrirSuporte() {
@@ -20,3 +22,4 @@ function abrirSuporte() {
 function fecharPWA() {
  window.close();
 }
+
