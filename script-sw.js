4deedc121a2c4e97d617ba3f9c2e1ec1eb6d5189
Waltemar Carneiro <waltemarcarneiro@gmail.com>
2025-06-08 06:58:57 -0400

    update

--- script-sw.js
+++ script-sw.js
@@ -44,35 +44,3 @@ if ('serviceWorker' in navigator) {
         }
     });
 }
-
-// Função para mostrar o toast de atualização
-function showUpdateToast() {
-  fetch('/update-toast.html')
-    .then(response => response.text())
-    .then(html => {
-      // Remover toast anterior se existir
-      const oldToast = document.getElementById('update-notification');
-      if (oldToast) oldToast.remove();
-
-      // Adicionar novo toast
-      const div = document.createElement('div');
-      div.innerHTML = html;
-      document.body.appendChild(div.querySelector('#update-notification'));
-
-      // Mostrar notificação
-      const notification = document.getElementById('update-notification');
-      notification.classList.remove('hidden');
-
-      // Configurar botões
-      document.getElementById('update-now').addEventListener('click', () => {
-        if (navigator.serviceWorker.controller) {
-          navigator.serviceWorker.controller.postMessage({ action: 'skipWaiting' });
-        }
-      });
-
-      document.getElementById('update-later').addEventListener('click', () => {
-        notification.classList.add('hidden');
-      });
-    })
-    .catch(error => console.error('Erro ao carregar toast:', error));
-}
