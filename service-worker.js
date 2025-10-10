33d7f029dac7a1d073b797c6edfdd78109b06e9d
Waltemar Carneiro <78711339+waltemarcarneiro@users.noreply.github.com>
2025-10-10 00:54:08 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'waltemar-v4.2.0';
+const CACHE_NAME = 'waltemar-v4.2.1';
 const MAX_CACHE_SIZE = 50 * 1024 * 1024; 
 const MAX_CACHE_AGE = 7 * 24 * 60 * 60 * 1000; 
 
@@ -242,3 +242,4 @@ self.addEventListener('notificationclick', event => {
   }
 });
  
+
